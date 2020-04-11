fcb3264766fc65f490b9a31f56f8888381c33c7d
Rohan-0 <48748727+Rohan-0@users.noreply.github.com>
2020-04-11 11:08:40 +0530

    Update app.js

--- public/AddCamp/app.js
+++ public/AddCamp/app.js
@@ -1,14 +1,40 @@
 $(document).ready(function(){
+
     var btn=document.getElementById('btnSubmit');
+    var id = document.getElementById('ID');
+    console.log(id.value);
+
+
     btn.onclick=function(){
         
-        var id = document.getElementById('ID');
         var name = document.getElementById("Name");
         var area = document.getElementById("Area");
-        var zipCode = document.getElementById("zipCode")
-        var rows = []
-        var count = 0
+        var zipCode = document.getElementById("zipCode");
+        var rows = [];
+        var count = 0;
         var length;
+
+        var table = document.createElement('table');
+        table.setAttribute('class','table table-bordered');
+        var divTable = document.getElementById('bbtable');
+        var h = document.createElement('h2');
+        let heading = document.createTextNode("CSV File Data");
+        h.appendChild(heading);
+        divTable.appendChild(h);
+        var arr = ['Donor_id','BloodCampId','Gender','BloodGrp','Age','MonthsLastDon','TotDon','VolDon'
+            ,'MonthsFirstDon','BloodDonNxt'];
+        var thead = document.createElement('thead');
+        var trHead = document.createElement('tr');
+        for(let i=0;i<arr.length;i++){
+            let tdHead=document.createElement('th');
+            let textNode=document.createTextNode(arr[i]);
+            tdHead.appendChild(textNode);
+            trHead.appendChild(tdHead);
+        }
+        thead.appendChild(trHead);
+        table.appendChild(thead);
+
+
         InsertBloodCamp();
         $('#files').parse({
             config: {
@@ -18,7 +44,7 @@ $(document).ready(function(){
         });
   
         
-        async function parseFileValues(results){
+        function parseFileValues(results){
             var data = results.data;
             length=results.data.length
             for(i=1;i<data.length;i++){
@@ -30,6 +56,7 @@ $(document).ready(function(){
             }
             console.log(rows);
         }
+
 
         function callApiForPred(col1,col2,col3,col4,i){
             $.ajax({
@@ -47,11 +74,15 @@ $(document).ready(function(){
                         console.log(rows);
                         insertRows(rows);
                     }
-                }});
+                }
+            })
+            return false
         }
+
+
         function insertRows(rows){
             for(var i=0;i<rows.length;i++){
-                var rowIns={Donor_id:rows[i][0],BloodCampId:id.value,BloodGrp:rows[i][1],Age:rows[i][2],Gender:rows[i][3],MonthsLastDon:rows[i][4],
+                var rowIns={Donor_id:rows[i][0],BloodCampId:id.value,Gender:rows[i][1],BloodGrp:rows[i][2],Age:rows[i][3],MonthsLastDon:rows[i][4],
                     TotDon:rows[i][5], VolDon:rows[i][6], MonthsFirstDon:rows[i][7], BloodDonNxt:rows[i][8]}
                 $.ajax({
                     url:`/insertRowDonor`,
@@ -61,19 +92,41 @@ $(document).ready(function(){
                         console.log("Inserted");
                     }
                 })
+
+                let tr = document.createElement('tr');
+                for(let j=0;j<9;j++){
+                    let td = document.createElement('td')
+                    let text = document.createTextNode(rows[i][j])
+                    td.appendChild(text)
+                    tr.append(td)
+                    if (j==0) {
+                        let td = document.createElement('td')
+                        let text = document.createTextNode(id.value)
+                        td.appendChild(text)
+                        tr.append(td)
+                    }
+                }
+                table.appendChild(tr)
             }
+            divTable.appendChild(table)
+            return false;
         }
+
+
         function InsertBloodCamp(){
+            console.log("BloodBank")
             var data={BloodCampId:id.value,Name:name.value,Area:area.value,zipCode:zipCode.value}
             $.ajax({
                 url:`/insertRowCamp`,
                 method:"POST",
                 data:data,
                 success: function(result){
-                    console.log("Camp Value Inserted")
+                    console.log(result)
                 }
             })
+            return false;
         }
+        return false;
     }
     
 })
