aea49b3cbca02c08c5a21f6d6ad600877188a1cc
Rohan-0 <48748727+Rohan-0@users.noreply.github.com>
2020-04-13 20:19:28 +0530

    Update app.js

--- public/Query/app.js
+++ public/Query/app.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
     let btn = document.getElementById("btnSubmit")
     var resultRecieved = false
+    let resultsID = []
+    let queryBtn = document.getElementById('QueryBtn')
+
     btn.onclick = function(){
+        let x = document.getElementById("closebb");
+        x.style.display = "block";
+
         let hID = document.getElementById("hospitalId")
         let name = document.getElementById("name")
         getResults(hID.value,name.value)
-        
         
         function getResults(hID,name){
             $.ajax({
@@ -36,6 +41,7 @@ $(document).ready(function(){
                         console.log(result[i])
                         li.appendChild(text)
                         list.appendChild(li)
+                        resultsID.push(result[i].BloodCampId)
                     }
                     resultRecieved = true
                 },
@@ -45,3 +51,62 @@ $(document).ready(function(){
             })
         }
     }
+
+
+    queryBtn.onclick = function(){
+        let count =0;
+        if(resultRecieved==false){
+            alert("Please Enter the hospital details")
+            return ;
+        }
+        let age = document.getElementById('Age')
+        let gender = document.getElementById('Gender')
+        let bloodGrp = document.getElementById('bloodGrp')
+        let answer = 0
+        let index;
+        let len = resultsID.length
+        for(let i=0; i<resultsID.length;i++){
+            countDonors(i)
+        }
+
+        function countDonors(i){
+            $.ajax({
+                url:'/countDonors',
+                method:"POST",
+                data:{age:age.value,gender:gender.value,bloodGrp:bloodGrp.value,BloodCampId:resultsID[i]},
+                success:function(result){
+                    if(parseInt(result)>parseInt(answer)){
+                        index=i+1;
+                        answer=result;
+                    }
+                    count++;
+                    if(count==len)
+                        displayResult()
+                },
+                error:function(e){
+                    console.log(e)
+                }
+            })
+        }
+
+        function displayResult(){
+            let ans = document.getElementById('Ans')
+            ans.style.display = "block";
+            $.ajax({
+                url:'/getBloodCamp',
+                method:"POST",
+                data:{ID:index},
+                success:function(result){
+                    let text = document.createTextNode(result[0].BloodCampId + " " + result[0].Name)
+                    ans.appendChild(text)
+                    ans.appendChild(document.createElement("br"))
+                },
+                error:function(e){
+                    console.log(e)
+                }
+            })
+        }
+        
+    }
+
+})
